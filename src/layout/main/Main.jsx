a9504b8cba62296724f794efc52891fa070cc597
shariar17e <shariarhossain23@gmail.com>
2023-01-13 13:01:35 +0600

    added logo on navbar

--- src/layout/main/Main.jsx
+++ src/layout/main/Main.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 const Main = () => {
   return (
     <div>
+      <marquee className="text-[40px] font-[700] text-black">গাবুরা রক্তদান সংস্থা  <span className="ml-16">স্থাপিত:২০১৮ ইং</span></marquee>
       <Navbar />
     </div>
   );
--- src/layout/main/Navbar.jsx
+++ src/layout/main/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import logo from "../../assets/gbo_logo-removebg-preview.png";
 
 const Navbar = () => {
-    return (
-        <nav>
-            this is navbar
-        </nav>
-    );
+  return (
+    <nav className="px-[5%] w-[100%] bg-primaryBG py-1">
+      <div className="">
+        <div className="flex items-center">
+          <img className="w-14" src={logo} alt="" />
+          <div>
+            <h1 className="text-white text-[15px] font-[700]">গাবুরা</h1>
+            <h1 className="text-white text-[15px] font-[700]">
+              রক্তদান সংস্থা
+            </h1>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

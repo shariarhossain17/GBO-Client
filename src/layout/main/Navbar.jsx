64b6a2090a3ef0a7d605562610205a1ee5476136
shariar17e <shariarhossain23@gmail.com>
2023-01-13 16:10:54 +0600

    toggle

--- src/layout/main/Navbar.jsx
+++ src/layout/main/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/gbo_logo-removebg-preview.png";
 import CustomNavlink from "../../components/shared/CustomNavlink";
 
 const Navbar = () => {
+    const [toggle,setToggle] = useState(false);
+
+    const handleToggle = ()=> {
+        setToggle(!toggle)
+    }
+
+    console.log(toggle);
   return (
     <nav className="px-[5%] w-[100%]  py-1 border-b">
-      <div className="flex justify-between items-center">
+      <div className="md:flex justify-between items-center">
         <div className="flex items-center">
           <img className="w-14" src={logo} alt="" />
           <div>
@@ -15,10 +22,11 @@ const Navbar = () => {
             <h1 className="text-[#171C2A] text-[15px] font-[700] ">
               রক্তদান সংস্থা
             </h1>
+            <button onClick={handleToggle} className="btn">MENu</button>
           </div>
         </div>
-        <div className="flex justify-between gap-8">
-          <ul className="flex items-center gap-6">
+        <div className={`md:flex justify-between gap-8 ${toggle ? "hidden":null}`}>
+          <ul className="md:flex items-center gap-6">
             <li className="text-textColor text-[14px] font-[500]">
               <CustomNavlink to="/">হোম</CustomNavlink>
             </li>
@@ -29,7 +37,9 @@ const Navbar = () => {
               <CustomNavlink to="/event">ইভেন্ট</CustomNavlink>
             </li>
           </ul>
-          <button className="bg-btnColor text-white px-2  py-1 rounded-full text-[14px]">লগইন</button>
+          <button className="bg-btnColor text-white px-2  py-1 rounded-full text-[14px]">
+            লগইন
+          </button>
         </div>
       </div>
     </nav>

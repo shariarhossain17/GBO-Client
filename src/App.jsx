981fb05c3a38c1247e5251cc581199f6b1ff8c44
shariar17e <shariarhossain23@gmail.com>
2023-01-13 12:08:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className='bg-primaryBG h-[80vh]'>
+     <button className='bg-secondary2'>Dark</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        textColor: "#868E9A",
+        buttonColor: "#0C1322",
+        accent: "#5798F6",
+
+        //dark
+        primaryBG: "#0F1629",
+      },
+    },
+  },
+  plugins: [],
+};
